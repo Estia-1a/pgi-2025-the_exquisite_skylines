a094ea61aadc4c5da68a9452582867d1a9c3fc80
mattismendi <mattis.mendiboure@etu.estia.fr>
2025-06-18 17:29:40 +0200

    En cours #9

--- src/main.c
+++ src/main.c
@@ -62,6 +62,6 @@ int main(int argc, char **argv) {
     color_desaturate(configuration.filenames[0]);
   } else if (strncmp(configuration.command, "rotate_cw", 9) == 0) {
     rotate_cw(configuration.filenames[0]);
-}
+  }
    return 0;
 }
